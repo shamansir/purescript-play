2e4c507f6d6e8eaa3d4013ce6be218f31541f02d
shaman.sir <shaman.sir@gmail.com>
2025-12-01 12:37:01 +0100

    extract the layouting algorithm to a separate module in source

--- src/Play/Play.purs
+++ src/Play/Play.purs
@@ -26,15 +26,12 @@ module Play
 
 import Prelude
 
-import Data.Array (length, filter, snoc) as Array
-import Data.Foldable (class Foldable, foldl)
-import Data.Int (toNumber) as Int
+import Data.Foldable (class Foldable)
 import Data.Traversable (class Traversable)
-import Data.Tuple (snd) as Tuple
-import Data.Tuple.Nested ((/\), type (/\))
-import Play.Types (Def, Direction(..), Offset, Padding, Pos, Rect, Size, Sizing(..), WithDef, WithDefRect, WithDefSize, WithRect) as PT
 import Yoga.Tree (Tree)
-import Yoga.Tree.Extended (node, break, flatten, value, children, update) as Tree
+import Yoga.Tree.Extended (node, flatten, value, children, update) as Tree
+import Play.Types (Def, Direction(..), Offset, Padding, Pos, Rect, Size, Sizing(..), WithDef, WithDefRect, WithDefSize, WithRect) as PT
+import Play.Layout (layoutTree) as Layout
 
 -- | A layout tree containing elements of type `a` with layout definitions.
 -- | This is the primary type for building layouts before they are computed.
@@ -93,15 +90,6 @@ infixl 1 playProp as ~*
 playProp :: forall x a. x -> (x -> Play a) -> Play a
 playProp = (#)
 
-
--- | Internal helper type for distinguishing between width and height calculations.
-data Side_
-    = Width
-    | Height
-
-
-derive instance Eq Side_
-
 -- | Compute the final layout from a Play tree specification.
 -- | This performs a three-phase layout computation:
 -- |
@@ -117,158 +105,7 @@ derive instance Eq Side_
 -- | computedLayout = Play.layout myPlay :: Layout String
 -- | ```
 layout :: forall a. Play a -> Layout a
-layout =
-    toTree
-        >>> Tree.break doFitSizing
-        >>> Tree.break doGrowSizing
-        >>> Tree.break (doPositioning { x : 0.0, y : 0.0 })
-        >>> Layout
-    where
-
-        rect :: PT.Pos -> PT.Size -> PT.Rect
-        rect pos size = { pos, size }
-
-        doFitSizing :: PT.WithDef a -> Array (Tree (PT.WithDef a)) -> Tree (PT.WithDefSize a)
-        doFitSizing { v, def } xs =
-            let
-                childrenSizes :: Array (Tree (PT.WithDefSize a))
-                childrenSizes = Tree.break doFitSizing <$> xs
-
-                childrenCount = Array.length xs :: Int
-
-                foldFMain :: (PT.Size -> Number) -> Number -> Tree (PT.WithDefSize a) -> Number
-                foldFMain extract n tree = n +   (tree # Tree.value # _.size # extract)
-
-                foldFSec  :: (PT.Size -> Number) -> Number -> Tree (PT.WithDefSize a) -> Number
-                foldFSec  extract n tree = max n (tree # Tree.value # _.size # extract)
-
-                fitAtSide :: Side_ -> Number
-                fitAtSide = case _ of
-                    Width ->
-                        case def.direction of
-                            PT.LeftToRight -> foldl (foldFMain _.width)  0.0 childrenSizes + (def.padding.left + def.padding.right  + (def.childGap * Int.toNumber (childrenCount - 1)))
-                            PT.TopToBottom -> foldl (foldFSec  _.width)  0.0 childrenSizes + (def.padding.left + def.padding.right)
-                    Height ->
-                        case def.direction of
-                            PT.LeftToRight -> foldl (foldFSec  _.height) 0.0 childrenSizes + (def.padding.top  + def.padding.bottom)
-                            PT.TopToBottom -> foldl (foldFMain _.height) 0.0 childrenSizes + (def.padding.top  + def.padding.bottom + (def.childGap * Int.toNumber (childrenCount - 1)))
-
-                calcSide :: Side_ -> Number
-                calcSide side =
-                    let
-                        sizing
-                            = case side of
-                                Width  -> def.sizing.width
-                                Height -> def.sizing.height
-                    in case sizing of
-                        PT.Fixed n -> n
-                        PT.Fit -> fitAtSide side
-                        PT.FitGrow -> fitAtSide side
-                        PT.FitMin fit -> max fit.min $ fitAtSide side
-                        PT.FitMax fit -> min fit.max $ fitAtSide side
-                        PT.FitMinMax fit -> min fit.max $ max fit.min $ fitAtSide side
-                        PT.Grow -> 0.0
-                        PT.GrowMin grow -> grow.min
-                        PT.None -> 0.0
-
-                size =
-                    let
-                        calcWidth  = calcSide Width
-                        calcHeight = calcSide Height
-                    in { width : calcWidth, height : calcHeight }
-            in
-                Tree.node
-                    { v, def, size }
-                    $ childrenSizes
-
-        doGrowSizing :: PT.WithDefSize a -> Array (Tree (PT.WithDefSize a)) -> Tree (PT.WithDefSize a)
-        doGrowSizing { v, def, size } children =
-            let
-                isGrowingSide :: PT.Sizing -> Boolean
-                isGrowingSide = case _ of
-                    PT.Grow -> true
-                    PT.GrowMin _ -> true
-                    PT.FitGrow -> true
-                    _ -> false
-                hasGrowingSide :: Side_ -> PT.Def -> Boolean
-                hasGrowingSide Width = _.sizing >>> _.width >>> isGrowingSide
-                hasGrowingSide Height = _.sizing >>> _.height >>> isGrowingSide
-                childrenCount = Array.length children
-                growChildrenCount side =
-                    Array.length
-                         $  Array.filter (hasGrowingSide side)
-                         $  (Tree.value >>> _.def)
-                        <$> children
-                growChildrenByW = growChildrenCount Width
-                growChildrenByH = growChildrenCount Height
-            in if growChildrenByW > 0 || growChildrenByH > 0 then
-                let
-                    growWidth = case def.direction of
-                        PT.LeftToRight ->
-                            let knownWidth = (foldl (+) 0.0 $ (Tree.value >>> _.size >>> _.width ) <$> children) + def.padding.left + def.padding.right  + (def.childGap * (Int.toNumber $ childrenCount - 1))
-                            in (size.width - knownWidth)  / Int.toNumber growChildrenByW
-                        PT.TopToBottom ->
-                            size.width - def.padding.right - def.padding.left
-                    growHeight = case def.direction of
-                        PT.LeftToRight ->
-                            size.height - def.padding.top - def.padding.bottom
-                        PT.TopToBottom ->
-                            let knownHeight = (foldl (+) 0.0 $ (Tree.value >>> _.size >>> _.height) <$> children) + def.padding.top  + def.padding.bottom + (def.childGap * (Int.toNumber $ childrenCount - 1))
-                            in (size.height - knownHeight) / Int.toNumber growChildrenByH
-                    addGrowingToChild :: Tree (PT.WithDefSize a) -> Tree (PT.WithDefSize a)
-                    addGrowingToChild child =
-                        case Tree.value child of
-                            ch ->
-                                let
-                                    addWidth  s =
-                                        if ch.def.sizing.width == PT.Grow then s { width = growWidth }
-                                        else if ch.def.sizing.width == PT.FitGrow then s { width = max s.width growWidth }
-                                        else s
-                                    addHeight s =
-                                        if ch.def.sizing.height == PT.Grow then s { height = growHeight }
-                                        else if ch.def.sizing.height == PT.FitGrow then s { height = max s.height growHeight }
-                                        else s
-                                in
-                                    Tree.update (\chv -> chv { size = addHeight $ addWidth chv.size }) child
-                in Tree.node { v, def, size } $ Tree.break doGrowSizing <$> addGrowingToChild <$> children
-            else
-                Tree.node { v, def, size } $ Tree.break doGrowSizing <$> children
-
-        doPositioning :: PT.Pos -> PT.WithDefSize a -> Array (Tree (PT.WithDefSize a)) -> Tree (PT.WithDefRect a) -- could be `WithRect` easily, but we keep `def` to be able to roll back `Layout` to original `Play`
-        doPositioning pos { v, def, size } xs =
-            Tree.node
-                { v, def, rect : rect pos size }
-                $ Tuple.snd $ foldl foldF (withPadding pos /\ []) xs
-            where
-                withPadding padPos = { x : padPos.x + def.padding.left, y : padPos.y + def.padding.top }
-
-                foldF
-                    :: PT.Offset /\ Array (Tree (PT.WithDefRect a))
-                    -> Tree (PT.WithDefSize a)
-                    -> PT.Offset /\ Array (Tree (PT.WithDefRect a))
-                foldF (offset /\ prev) chTree =
-                    let
-                        curVal  = _.v    $ Tree.value chTree :: a
-                        curDef  = _.def  $ Tree.value chTree :: PT.Def
-                        curSize = _.size $ Tree.value chTree :: PT.Size
-                        -- childrenCount = Array.length $ Tree.children chTree :: Int
-                        nextOffset =
-                            case def.direction of
-                                PT.LeftToRight ->
-                                    { x : offset.x + curSize.width + def.childGap
-                                    , y : offset.y
-                                    }
-                                PT.TopToBottom ->
-                                    { x : offset.x
-                                    , y : offset.y + curSize.height + def.childGap
-                                    }
-                            :: PT.Offset
-                        nextNode :: Tree (PT.WithDefRect a)
-                        nextNode =
-                            (Tree.node { v : curVal, def : curDef, size : curSize } $ Tree.children chTree)
-                                # Tree.break (doPositioning offset)
-                    in nextOffset
-                        /\ Array.snoc prev nextNode
+layout = toTree >>> Layout.layoutTree >>> Layout
 
 
 -- | Extract the underlying tree structure from a Play layout.
